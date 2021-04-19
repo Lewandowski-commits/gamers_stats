eb0fbbbcdfc7b1db0b87793f82288da21ad39731
lewandowskicommits <vesilth@gmail.com>
2021-04-20 01:08:05 +0200

    add flash messages and my name into footer

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash
 from api import *
 
 df = get_all_apps()
 
 app = Flask(__name__)
+app.secret_key = '125asf'
 
 @app.route('/')
 def hello_world():
@@ -24,11 +25,15 @@ def game(game_name):
     filtered_df = df[df['name_lower'].str.contains(game_name)]
     filtered_df = filtered_df.iloc[:,0:1].sort_values('name')
 
-    return render_template('searchresults.html',
-                           column_names=filtered_df.columns.values,
-                           row_data=list(filtered_df.values.tolist()),
-                           zip=zip
-                           )
+    if filtered_df.shape[0] == 0:
+        flash('No games match your search')
+        return redirect('/')
+    else:
+        return render_template('searchresults.html',
+                               column_names=filtered_df.columns.values,
+                               row_data=list(filtered_df.values.tolist()),
+                               zip=zip
+                               )
 
 if __name__ == '__main__':
    app.run(debug=True)
