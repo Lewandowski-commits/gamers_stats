47ab5dd31a26f2d17185fc039730a74de41e59ab
lewandowskicommits <vesilth@gmail.com>
2021-04-21 00:48:48 +0200

    fixed indents

--- api.py
+++ api.py
@@ -2,10 +2,10 @@ import requests as re
 
 def get_app_details(app_name):
     try:
-    _config_file = open('config.txt', 'r')
-    api_key = _config_file.readline()
+        _config_file = open('config.txt', 'r')
+        api_key = _config_file.readline()
     except:
-    api_key = str(os.environ.get('API_KEY', 3))
+        api_key = str(os.environ.get('API_KEY', 3))
 
     url = f'https://api.rawg.io/api/games?key={api_key}&search_precise=true&search=%27{app_name.lower()}%27'
     call = re.get(url).json()
