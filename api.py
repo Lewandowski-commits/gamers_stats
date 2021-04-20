c5d4c3cb78b73cd5e27aacfff9da7a3c80f22a03
lewandowskicommits <vesilth@gmail.com>
2021-04-20 22:47:43 +0200

    added api key pulling from config.txt

--- api.py
+++ api.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import requests as re
 
+_config_file = open('config.txt', 'r')
+api_key = _config_file.readline()
+
 def get_all_apps():
     all_steam_apps = re.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/').json()['applist']['apps']
     df_apps = pd.DataFrame(all_steam_apps)
     df_apps['name_lower'] = df_apps['name'].apply(lambda x: x.lower())
     return df_apps
+
+def get_app_details(appid):
+
+    return None
