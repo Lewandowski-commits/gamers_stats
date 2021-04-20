56d64c4a70d9a3167b7ba093d2542a86146a7d24
lewandowskicommits <vesilth@gmail.com>
2021-04-21 00:48:03 +0200

    change heroku config variable to str

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ def get_app_details(app_name):
     _config_file = open('config.txt', 'r')
     api_key = _config_file.readline()
     except:
-    api_key = int(os.environ.get('API_KEY', 3))
+    api_key = str(os.environ.get('API_KEY', 3))
 
     url = f'https://api.rawg.io/api/games?key={api_key}&search_precise=true&search=%27{app_name.lower()}%27'
     call = re.get(url).json()
